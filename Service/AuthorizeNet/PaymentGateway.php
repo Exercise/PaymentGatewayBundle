a9e8f460a63eec0334236884e461c72e6b22b64f
tirnanog06 <matthewfitz@gmail.com>
2010-10-14 19:36:10 -0400

    Add basic methods for configuration

--- Service/AuthorizeNet/PaymentGateway.php
+++ Service/AuthorizeNet/PaymentGateway.php
@@ -9,21 +9,39 @@ use Bundle\PaymentGatewayBundle\Abstract\PaymentMethod;
 
 class PaymentGateway extends AbstractPaymentGateway {
 	
+	CONST VERSION_KEY        = "x_version";
+	CONST DELIM_DATA_KEY     = "x_delim_data";
+	CONST DELIM_CHAR_KEY     = "x_delim_char";
+	CONST RELAY_RESPONSE_KEY = "x_relay_response";
+
 	private $address;
 	private $paymentMethod;
 	private $order;
 	private $amount;
 	
-	public function __construct() {
+	private $configs = array(
+		"version"              => "3.1",
+		"delim_data"           => TRUE,
+		"delim_char"           => "|",
+		"relay_response"       => FALSE,
+		"curl_header"          => 0, // set to 0 to eliminate header info from response
+		"curl_return_transfer" => 1, // Returns response data instead of TRUE(1)
+		"curl_ssl_verify_peer" => FALSE,
+	);
 
+	private $curl;
+	private $response;
+	
+	public function __construct(array $configs = array()) {
+		$this->configs = $configs;
 	}
 
 	private function connect() {
-
+		
 	}
 
 	private function disconnect() {
-
+		curl_close($this->getCurl());
 	}
 
 	public function authorize() {
@@ -68,6 +86,93 @@ class PaymentGateway extends AbstractPaymentGateway {
 
 	public function getAmount() {
 		return $this->amount;
+	}
+
+	public function getApiLoginId() {
+		if (array_key_exists('apiLoginId', $this->config)) {
+			return $this->config['apiLoginId'];
+		}
+	}
+
+	public function setApiLoginId($apiLoginId) {
+		return $this->config['apiLoginId'] = (string) $apiLoginId;
+	}
+
+	public function getTransactionKey() {
+		if (array_key_exists('transactionKey', $this->config)) {
+			return $this->config['transactionKey'];
+		}
+	}
+
+	public function setTransactionKey($transactionKey) {
+		return $this->config['transactionKey'] = (string) $transactionKey;
+	}
+	
+	public function getVersion() {
+		if (array_key_exists('version', $this->config)) {
+			return $this->config['version'];
+		}
+	}
+
+	public function setVersion($version) {
+		return $this->config['version'] = (string) $version;
+	}
+
+	public function getDelimData() {
+		if (array_key_exists('delimData', $this->config)) {
+			return $this->config['delimData'];
+		}
+	}
+
+	public function setDelimData($delimData) {
+		return $this->config['delimData'] = (bool) $delimData;
+	}
+
+	public function getDelimChar() {
+		if (array_key_exists('delimChar', $this->config)) {
+			return $this->config['delimChar'];
+		}
+	}
+
+	public function setDelimChar($delimChar) {
+		return $this->config['delimChar'] = (string) $delimChar;
+	}
+
+	public function getRelayResponse() {
+		if (array_key_exists('relayResponse', $this->config)) {
+			return $this->config['relayResponse'];
+		}
+	}
+
+	public function setRelayResponse($relayResponse) {
+		return $this->config['relayResponse'] = (bool) $relayResponse;
+	}
+	
+	public function getPostUrl() {
+		if (array_key_exists('postUrl', $this->config)) {
+			return $this->config['postUrl'];
+		}
+	}
+
+	public function setPostUrl($postUrl) {
+		return $this->config['postUrl'] = (string) $postUrl;
+	}
+
+	public function getCurl() {
+		return $this->curl;
+	}
+
+	public function setCurl($postUrl = null) {
+		if (!isset($postUrl)) $postUrl = $this->getPostUrl();
+		$this->curl = curl_init($postUrl);
+	}
+
+	public function getResponse() {
+		return $this->response;
+	}
+
+	private function curlExec() {
+		$this->response = curl_exec($this->getCurl());
 	}
 
 }
